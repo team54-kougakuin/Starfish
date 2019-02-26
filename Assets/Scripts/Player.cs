36a351f48d463d1960b3d91b8c1aeb97b6895bd7
erikusia <erikusiamk2@gmail.com>
2019-02-26 11:13:28 +0900

    2/22最終コミット

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,11 +6,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed = 5.0f;
+
+    public float jumpPower = 700;//ジャンプ力
+    public LayerMask groundLayer;//Linecastで判定するLayer
+    bool isGround;//着地判定
+
+    Rigidbody2D rb;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,8 +27,23 @@ public class Player : MonoBehaviour
         var input = Input.GetAxisRaw("Horizontal");
         var component = GetComponent<Rigidbody2D>();
         var velocity = component.velocity;
-        velocity.x = input * 5f;
+        velocity.x = input * speed;
         component.velocity = velocity;
 
+        //LinecastでPlayerの足元に地面があるか判定
+        isGround = Physics2D.Linecast(
+            transform.position + transform.up * 1,
+            transform.position - transform.up * 0.05f,
+            groundLayer);
+        //スペースキーを押し、さらに接地しているとき
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            //着地判定をfalse
+            isGround = false;
+            //AddForceにて上方向へ力を加える
+            rb.AddForce(Vector2.up * jumpPower);
+        }
+
+        Debug.Log(isGround);
     }
 }
