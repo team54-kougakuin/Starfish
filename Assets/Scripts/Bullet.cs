eed4470d762c197947f5ffc1b49d0ef7517327e0
根岸 渉 <teteruru3690@gmail.com>
2019-04-11 11:26:27 +0900

    画面の枠を制作 弾を発射できるように、弾が何かに当たったら止まるように

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private bool isHit = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isHit)
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isHit = true;
+    }
+}
