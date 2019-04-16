07ca3da36a5d28aa86d81c9f2557d52587df6c03
TakanoKakeru <llmn.0419@gmail.com>
2019-04-16 15:47:38 +0900

    横移動エネミー修正

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,21 +5,22 @@ using UnityEngine;
 public class Enemy : MonoBehaviour {
     public float speed = 1;
     private float vx = 1;
-    private float rightLeft;
+    private float right, left;
     private GameObject ground;
 
     // Start is called before the first frame update
     void Start() {
-        ground = GameObject.Find("Ground");
-        //地面の中央から端までの長さを求める
-        rightLeft = ground.transform.localPosition.x + ground.transform.localScale.x / 2;
+        ground = GameObject.Find("Grid");
+        //Position + 地面の中央から端までの長さ
+        right = ground.transform.position.x + ground.transform.localScale.x / 2;
+        left = ground.transform.position.x - ground.transform.localScale.x / 2;
     }
 
     // Update is called once per frame
     void Update() {
         //地面端まで行ったら移動量反転
-        if (gameObject.transform.localPosition.x + gameObject.transform.localScale.x / 2 > rightLeft
-            || gameObject.transform.localPosition.x - gameObject.transform.localScale.x / 2 < -rightLeft) {
+        if (gameObject.transform.position.x + gameObject.transform.localScale.x / 2 > right ||
+            gameObject.transform.position.x - gameObject.transform.localScale.x / 2 < left) {
             vx *= -1;
         }
 
@@ -27,13 +28,14 @@ public class Enemy : MonoBehaviour {
         Vector2 direction = new Vector2(vx, 0).normalized;
 
         // 移動する向きとスピードを代入する
-        GetComponent<Rigidbody>().velocity = direction * speed;
+        //GetComponent<Rigidbody>().velocity = direction * speed;
+        GetComponent<Rigidbody2D>().velocity = direction * speed;
     }
 
-    void OnTriggerEnter(Collider other) {
+    void OnTriggerEnter2D(Collider2D collision) {
         //ぶつかったのがプレイヤーだったらプレイヤーを削除
-        if (other.tag == "Player") {
-            Destroy(other.gameObject);
+        if (collision.tag == "Player") {
+            Destroy(collision.gameObject);
         }
 
         //障害物にぶつかったら移動量反転
